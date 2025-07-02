309a8739d6cc6a0f2a0f08a2af4223081a5e655a
Norbertmary Nduaguba <buikenduaguba@gmail.com>
2025-07-01 19:40:50 -0500

    finito

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -60,6 +60,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   return (
     <div className="cart-container">
+      <h2 style={{ color: "black" }}>Total Plants : {cart.length}</h2>
       <h2 style={{ color: "black" }}>
         Total Cart Amount: ${calculateTotalAmount()}
       </h2>
